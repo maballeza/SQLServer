4dbaca19c76497dc503d266de7a8427fe237d8d3
Miguel Balleza <43941578+maballeza@users.noreply.github.com>
2021-10-02 22:45:27 -0400

    List of abbreviation-state pairs is now inserted into the new table.

---
+++ definitions.py
@@ -0,0 +1,69 @@
+# Helper
+def Print(column_1, column_2) :
+    print(str.ljust(column_1, 20, ' ') + str.ljust(column_2, 20, ' '))
+
+# class Str is used to process arguments as state abbreviations;
+# these are then used as a mapping to the full name.
+class Str :
+    pass
+
+# Connection strings
+server = '<server>.database.windows.net'
+database = '<database>'
+username = '<username>'
+password = '<password>'
+driver = '{ODBC Driver 17 for SQL Server}'
+connection_string = f'DRIVER={driver}; SERVER={server};;DATABASE={database};UID={username};PWD={password};'
+
+states = [
+    ('Alabama', 'AL'),
+    ('Alaska', 'AK'),
+    ('Arizona', 'AZ'),
+    ('Arkansas', 'AR'),
+    ('California', 'CA'),
+    ('Colorado', 'CO'),
+    ('Connecticut', 'CT'),
+    ('Delaware', 'DE'),
+    ('Florida', 'FL'),
+    ('Georgia', 'GA'),
+    ('Hawaii', 'HI'),
+    ('Idaho', 'ID'),
+    ('Illinois', 'IL'),
+    ('Indiana', 'IN'),
+    ('Iowa', 'IA'),
+    ('Kansas', 'KS'),
+    ('Kentucky', 'KY'),
+    ('Louisiana', 'LA'),
+    ('Maine', 'ME'),
+    ('Montana', 'MT'),
+    ('Nebraska', 'NE'),
+    ('Nevada', 'NV'),
+    ('New Hampshire', 'NH'),
+    ('New Jersey', 'NJ'),
+    ('New Mexico', 'NM'),
+    ('New York', 'NY'),
+    ('North Carolina', 'NC'),
+    ('North Dakota', 'ND'),
+    ('Ohio', 'OH'),
+    ('Oklahoma', 'OK'),
+    ('Oregon', 'OR'),
+    ('Maryland', 'MD'),
+    ('Massachusetts', 'MA'),
+    ('Michigan', 'MI'),
+    ('Minnesota', 'MN'),
+    ('Mississippi', 'MS'),
+    ('Missouri', 'MO'),
+    ('Pennsylvania', 'PA'),
+    ('Rhode Island', 'RI'),
+    ('South Carolina', 'SC'),
+    ('South Dakota', 'SD'),
+    ('Tennessee', 'TN'),
+    ('Texas', 'TX'),
+    ('Utah', 'UT'),
+    ('Vermont', 'VT'),
+    ('Virginia', 'VA'),
+    ('Washington', 'WA'),
+    ('West Virginia', 'WV'),
+    ('Wisconsin', 'WI'),
+    ('Wyoming', 'WY')
+    ]
--- salebystate.py
+++ salebystate.py
@@ -1,146 +1,42 @@
 # A script based on Microsoft's sample: https://docs.microsoft.com/en-us/azure/azure-sql/database/connect-query-python
 # Note: Requires the creation of an Azure SQL Database: https://docs.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver15&tabs=ssms
-
-import argparse
 import pyodbc
-# The 'server', 'database', 'username', and 'password' variables need replacement.
-server = '<server>.database.windows.net'
-database = '<database>'
-username = '<username>'
-password = '<password>'   
-driver= '{ODBC Driver 17 for SQL Server}'
+from definitions import connection_string, states, Print
 
-abrv=[
-    'AL',
-    'AK',
-    'AZ',
-    'AR',
-    'CA',
-    'CO',
-    'CT',
-    'DE',
-    'FL',
-    'GA',
-    'HI',
-    'ID',
-    'IL',
-    'IN',
-    'IA',
-    'KS',
-    'KY',
-    'LA',
-    'ME',
-    'MT',
-    'NE',
-    'NV',
-    'NH',
-    'NJ',
-    'NM',
-    'NY',
-    'NC',
-    'ND',
-    'OH',
-    'OK',
-    'OR',
-    'MD',
-    'MA',
-    'MI',
-    'MN',
-    'MS',
-    'MO',
-    'PA',
-    'RI',
-    'SC',
-    'SD',
-    'TN',
-    'TX',
-    'UT',
-    'VT',
-    'VA',
-    'WA',
-    'WV',
-    'WI',
-    'WY',
-    ]
-states={
-    'AL' : 'Alabama',
-    'AK' : 'Alaska',
-    'AZ' : 'Arizona',
-    'AR' : 'Arkansas',
-    'CA' : 'California',
-    'CO' : 'Colorado',
-    'CT' : 'Connecticut',
-    'DE' : 'Delaware',
-    'FL' : 'Florida',
-    'GA' : 'Georgia',
-    'HI' : 'Hawaii',
-    'ID' : 'Idaho',
-    'IL' : 'Illinois',
-    'IN' : 'Indiana',
-    'IA' : 'Iowa',
-    'KS' : 'Kansas',
-    'KY' : 'Kentucky',
-    'LA' : 'Louisiana',
-    'ME' : 'Maine',
-    'MT' : 'Montana',
-    'NE' : 'Nebraska',
-    'NV' : 'Nevada',
-    'NH' : 'New Hampshire',
-    'NJ' : 'New Jersey',
-    'NM' : 'New Mexico',
-    'NY' : 'New York',
-    'NC' : 'North Carolina',
-    'ND' : 'North Dakota',
-    'OH' : 'Ohio',
-    'OK' : 'Oklahoma',
-    'OR' : 'Oregon',
-    'MD' : 'Maryland',
-    'MA' : 'Massachusetts',
-    'MI' : 'Michigan',
-    'MN' : 'Minnesota',
-    'MS' : 'Mississippi',
-    'MO' : 'Missouri',
-    'PA' : 'Pennsylvania',
-    'RI' : 'Rhode Island',
-    'SC' : 'South Carolina',
-    'SD' : 'South Dakota',
-    'TN' : 'Tennessee',
-    'TX' : 'Texas',
-    'UT' : 'Utah',
-    'VT' : 'Vermont',
-    'VA' : 'Virginia',
-    'WA' : 'Washington',
-    'WV' : 'West Virginia',
-    'WI' : 'Wisconsin',
-    'WY' : 'Wyoming'
-    }
-
-# class Str is used to process arguments as state abbreviations; these
-# are then used as a mapping to the full name.
-class Str :
-    pass
-s = Str()
-
-# The parser used to process the command line arguments.
-parser = argparse.ArgumentParser(
-    description='A script returning products sold based on the state in which the sale took place.')
-parser.add_argument(
-    '--st', choices=abrv,
-    help='An example: To see sales made in Alabama use \'python salebystate.py --st AL\'')
-args = parser.parse_args(namespace=Str)
+# Database schema and table creation strings.
+schema = 'Abbreviations'
+table = 'StateProvince'
+col_1 = 'Name'
+col_2 = 'Abbreviation'
 
 # The script connecting to the 'Adventure Works' database (see 'README.md' for more information).
-with pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+with pyodbc.connect(connection_string) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("CREATE SCHEMA Abbreviations")
-        conn.commit()
-        cursor.execute("CREATE TABLE Abbreviations.StateProvince ( Name VARCHAR(255) NOT NULL, Abbreviation VARCHAR(255) NOT NULL )")
-        conn.commit()
-        cursor.execute("INSERT INTO Abbreviations.StateProvince (Name, Abbreviation) VALUES ( N'Alaska', N'AL')")
-        conn.commit()
-        cursor.execute("SELECT TOP 10 a.Name, a.Abbreviation FROM Abbreviations.StateProvince as a ORDER BY a.Name ASC;")
-        row = cursor.fetchone()
-        print("State\t\t\tAbbreviation")
-        while row:
-            print(str(row[0]) + '\t\t\t' + str(row[1]))
-            row = cursor.fetchone()
+        # Debugging.
+        cursor.execute(f"""
+                        DROP TABLE IF EXISTS {schema}.{table};
+                        DROP SCHEMA IF EXISTS {schema};
+                        """)
+        # Add abbreviations for ease of future access.
+        cursor.execute(f"CREATE SCHEMA {schema};")
+        cursor.execute(f"""
+                        CREATE TABLE {schema}.{table}
+                        ({col_1} VARCHAR(255) NOT NULL,
+                        {col_2} VARCHAR(255) NOT NULL);
+                        """)
+        # Add values in states into cursor before sending them to the server.
+        cursor.executemany(f"""
+                            INSERT INTO {schema}.{table} ({col_1}, {col_2})
+                            VALUES (?, ?)
+                            """, states)
+
+        cursor.execute(f"""
+                        SELECT a.{col_1}, a.{col_2} 
+                        FROM {schema}.{table} as a 
+                        ORDER BY a.{col_1} ASC;
+                        """)
+        rows = cursor.fetchall()
+
+        Print(col_1, col_2)
+        for row in rows:
+            Print(row.Name, row.Abbreviation)
